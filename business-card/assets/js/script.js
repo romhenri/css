85b4ea709e960e6d0e12214c182050a94319762b
Rômulo Henri <123867521+romhenri@users.noreply.github.com>
2023-04-05 23:42:05 -0300

    Atuallizando

--- business-card/assets/js/script.js
+++
@@ -1,9 +0,0 @@
-const CMD = document.getElementById('buttonColorMode')
-const BODY = document.getElementById('body')
-CMD.addEventListener("click", change)
-
-function change() {
-    alert('Funcionou')
-
-    BODY.style.backgroundColor= "#ffff"
-}
